a5586b0e5bf9e9503383bb5166da99bda1495804
murzilinSin <asair4ik@mail.ru>
2021-11-28 20:04:21 +0500

    first experience of Compose

---
+++ app/src/main/java/com/example/composstart/SampleData.kt
@@ -0,0 +1,72 @@
+package com.example.composstart
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Ichigo Kurosaki",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "List of Android versions:\n" +
+                    "Android KitKat (API 19)\n" +
+                    "Android Lollipop (API 21)\n" +
+                    "Android Marshmallow (API 23)\n" +
+                    "Android Nougat (API 24)\n" +
+                    "Android Oreo (API 26)\n" +
+                    "Android Pie (API 28)\n" +
+                    "Android 10 (API 29)\n" +
+                    "Android 11 (API 30)\n" +
+                    "Android 12 (API 31)\n"
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "I think Kotlin is my favorite programming language.\n" +
+                    "It's so much fun!"
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Ichigo Kurosaki",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
